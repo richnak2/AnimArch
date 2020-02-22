9554a10ae1f7fda358e531c3c675e9782f570450
Neltharion59 <lukawh5959@gmail.com>
2020-02-22 19:28:27 +0100

    Preparation of Code skeleton and Unit tests for Andrej - New Code generation

---
+++ AnimationControl/MethodCall.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimationControl
+{
+    class MethodCall
+    {
+        public String CallerClass {get; set;}
+        public String CalledClass { get; set; }
+        public String CallerMethod { get; set; }
+        public String CalledMethod { get; set; }
+        public String RelationshipName { get; set; }
+        public Boolean NewSequence { get; set; }
+    }
+}
