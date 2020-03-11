86578dbc245074913dd6542be753213fbb97f843
Neltharion59 <lukawh5959@gmail.com>
2020-03-11 02:41:00 +0100

    Synchronization adjustments, attempt to make unit test

---
+++ AnimationControl/EXEThreadSynchronizator.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AnimationControl
+{
+    public class EXEThreadSynchronizator
+    {
+        private object Syncer { get; }
+        private int ThreadCount { get; set; }
+        private int UntilLockResetCount { get; set; }
+        private int InLockCount { get; set; }
+        private Boolean blocked { get; set; }
+        private List<int> ChangeQueue { get; }
+        public EXEThreadSynchronizator()
+        {
+            this.Syncer = new object();
+            this.ThreadCount = 0;
+            this.UntilLockResetCount = 0;
+            this.InLockCount = 0;
+            this.blocked = false;
+            this.ChangeQueue = new List<int>();
+        }
+
+        public void RegisterThread(uint count)
+        {
+            lock (this.Syncer)
+            {
+                ChangeQueue.Add((int)count);
+            }
+        }
+        public void UnregisterThread()
+        {
+            lock (this.Syncer)
+            {
+                ChangeQueue.Add(-1);
+            }
+        }
+        public void RequestStep()
+        {
+            lock (this.Syncer)
+            {
+                //If the room has been entered previously and has not been emptied yet, we must wait until it's emptied
+                while (this.blocked)
+                {
+                    Monitor.Wait(this.Syncer);
+                }
+
+                //If someone changed number of threads, let's do it
+                if (this.ChangeQueue.Any())
+                {
+                    int change = this.ChangeQueue.Sum();
+                    this.ThreadCount += change;
+                    this.UntilLockResetCount += change;
+                    ChangeQueue.Clear();
+                }
+
+                //Let us let know everyone of our presence
+                this.InLockCount++;
+                this.UntilLockResetCount--;
+                //If we have to wait for more threads, let's sleep until they come
+                while (this.UntilLockResetCount > 0)
+                {
+                    //If someone changed number of threads, let's do it
+                    if (this.ChangeQueue.Any())
+                    {
+                        int change = this.ChangeQueue.Sum();
+                        this.ThreadCount += change;
+                        this.UntilLockResetCount += change;
+                        ChangeQueue.Clear();
+                    }
+
+                    Monitor.Wait(this.Syncer);
+                }
+                //If we are the last thread to come, let's block the room until everyone leaves and wake everyone up
+                if (this.UntilLockResetCount == 0)
+                {
+                    this.blocked = true;
+                    Monitor.PulseAll(this.Syncer);
+                }
+
+                //Let know that we are leaving
+                this.InLockCount--;
+                //If we are the last one leaving, unblock the room and reset counters. And wake up possible sleepers
+                if (this.InLockCount == 0)
+                {
+                    this.blocked = false;
+                    this.UntilLockResetCount = this.ThreadCount;
+                    Monitor.PulseAll(this.Syncer);
+                }
+            }
+        }
+    }
+}
---
+++ AnimationControlTests/EXEThreadSynchronizatorTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace AnimationControl.Tests
+{
+    [TestClass]
+    public class EXEThreadSynchronizatorTests
+    {
+        [TestMethod]
+        public void EXEThreadSynchronizatorTest()
+        {
+            EXEThreadSynchronizator Syncer = new EXEThreadSynchronizator();
+            Syncer.RegisterThread(3);
+
+            object Locker = new object();
+            string ActualResult = "";
+            object OverLocker = new object();
+            int endedCount = 0;
+            new Thread(() => { for (int i = 0; i < 1; i++) { Syncer.RequestStep(); lock (Locker) { ActualResult += "A"; }} lock (OverLocker) { endedCount++; Monitor.Pulse(OverLocker); } }).Start();
+            new Thread(() => { for (int i = 0; i < 1; i++) { Syncer.RequestStep(); lock (Locker) { ActualResult += "B"; } } lock (OverLocker) { endedCount++; Monitor.Pulse(OverLocker); } }).Start();
+            new Thread(() => { for (int i = 0; i < 1; i++) { Syncer.RequestStep(); lock (Locker) { ActualResult += "C"; } } lock (OverLocker) { endedCount++; Monitor.Pulse(OverLocker); } }).Start();
+
+            lock (OverLocker)
+            {
+                while (endedCount != 0)
+                {
+                    Monitor.Wait(OverLocker);
+                }
+            }
+
+            StringAssert.Matches(ActualResult, new Regex(@"", RegexOptions.Compiled | RegexOptions.IgnoreCase));
+        }
+    }
+}
