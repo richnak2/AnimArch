644b3ed3e51b4205c851f669f93998f70b7dc7ee
Neltharion59 <lukawh5959@gmail.com>
2020-04-08 13:21:14 +0200

    Decorator of CallCommand for testing purposes, one test

---
+++ AnimationControl/EXECommandCallTestDecorator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimationControl
+{
+    public class EXECommandCallTestDecorator : EXECommand
+    {
+        private EXECommandCall DecoratedCommand { get; }
+        private StringBuffer CallTextBuffer { get; }
+        public EXECommandCallTestDecorator(EXECommandCall DecoratedCommand, StringBuffer CallTextBuffer)
+        {
+            this.DecoratedCommand = DecoratedCommand;
+            this.CallTextBuffer = CallTextBuffer;
+        }
+
+        public override bool Execute(Animation Animation, EXEScope Scope)
+        {
+            bool Result = DecoratedCommand.Execute(Animation, Scope);
+            if (!Result)
+            {
+                return Result;
+            }
+            CallTextBuffer.Append(DecoratedCommand.ToCode());
+            return Result;
+        }
+    }
+}
---
+++ AnimationControlTests/EXECommandCallTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnimationControl;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimationControl.Tests
+{
+    [TestClass]
+    public class EXECommandCallTests
+    {
+        [TestMethod]
+        public void Execute_Normal_01()
+        {
+            Animation Animation = new Animation();
+            CDClass ClassObserver = Animation.ExecutionSpace.SpawnClass("Observer");
+            ClassObserver.AddMethod(new CDMethod("init", "void"));
+            CDClass ClassSubject = Animation.ExecutionSpace.SpawnClass("Subject");
+            ClassSubject.AddMethod(new CDMethod("register", "bool"));
+            Animation.RelationshipSpace.SpawnRelationship("Observer", "Subject");
+
+            StringBuffer StringBuffer = new StringBuffer();
+
+            Animation.SuperScope.AddCommand(new EXECommandCallTestDecorator(
+                new EXECommandCall("Observer", "init", "R1", "Subject", "register"),
+                StringBuffer
+            ));
+
+            Boolean ExecutionSuccess = Animation.Execute();
+
+            String ExpectedCallHistory = "call from Observer::init() to Subject::register() across R1;\n";
+
+            String ActualCallHistory = StringBuffer.GenerateString();
+
+            Assert.IsTrue(ExecutionSuccess);
+            Assert.AreEqual(ExpectedCallHistory, ActualCallHistory);
+        }
+        [TestMethod]
+        public void Execute_Normal_02()
+        {
+            Animation Animation = new Animation();
+            CDClass ClassObserver = Animation.ExecutionSpace.SpawnClass("Observer");
+            ClassObserver.AddMethod(new CDMethod("init", "void"));
+            CDClass ClassSubject = Animation.ExecutionSpace.SpawnClass("Subject");
+            ClassSubject.AddMethod(new CDMethod("register", "bool"));
+            Animation.RelationshipSpace.SpawnRelationship("Observer", "Subject");
+
+            StringBuffer StringBuffer = new StringBuffer();
+
+            Animation.SuperScope.AddCommand(new EXECommandAssignment("x", new EXEASTNodeLeaf("0")));
+            Animation.SuperScope.AddCommand(new EXECommandCallTestDecorator(
+                new EXECommandCall("Observer", "init", "R1", "Subject", "register"),
+                StringBuffer
+            ));
+
+            Boolean ExecutionSuccess = Animation.Execute();
+
+            String ExpectedCallHistory = "call from Observer::init() to Subject::register() across R1;\n";
+
+            String ActualCallHistory = StringBuffer.GenerateString();
+
+            Assert.IsTrue(ExecutionSuccess);
+            Assert.AreEqual(ExpectedCallHistory, ActualCallHistory);
+        }
+    }
+}
