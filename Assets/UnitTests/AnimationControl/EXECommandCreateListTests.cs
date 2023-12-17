6b2d11a298dd6de8ba1bee5dcdc7ae8cda56a490
xEvickA <evickarosnicka@gmail.com>
2023-12-17 21:43:51 +0100

    Create tests for synonymous names of primitives
    Create test for lists of primitives with synonymous names (integer - int, etc.)

--- Assets/UnitTests/AnimationControl/EXECommandCreateListTests.cs
+++ Assets/UnitTests/AnimationControl/EXECommandCreateListTests.cs
@@ -13,7 +13,7 @@ namespace Assets.UnitTests.AnimationControl
 
         // Test int, double, bool, string 
         [Test]
-        public void HappyDay_01_AssignToLocalVariable_01_Literal_01_ListOfInts()
+        public void HappyDay_01_AssignToLocalVariable_01_Literal_01_ListOfIntegers()
         {
             CommandTest Test = new CommandTest();
 
@@ -49,7 +49,7 @@ namespace Assets.UnitTests.AnimationControl
         }
 
         [Test]
-        public void HappyDay_01_AssignToLocalVariable_01_Literal_01_ListOfFloats()
+        public void HappyDay_01_AssignToLocalVariable_01_Literal_01_ListOfReals()
         {
             CommandTest Test = new CommandTest();
 
@@ -140,6 +140,148 @@ namespace Assets.UnitTests.AnimationControl
 
             EXEExecutionResult _executionResult = PerformExecution(programInstance);
             EXEValueArray array = new EXEValueArray("boolean[]");
+            array.InitializeEmptyArray();
+            array.AppendElement(new EXEValueBool(false), programInstance.ExecutionSpace);
+            array.AppendElement(new EXEValueBool(true), programInstance.ExecutionSpace);
+
+            // Assert
+            Test.Declare(methodScope, _executionResult);
+
+            Test.Variables
+                .ExpectVariable("x", array)
+                .ExpectVariable("self", new EXEValueReference(methodScope.OwningObject));
+
+            Test.PerformAssertion();
+        }
+
+        [Test]
+        public void HappyDay_01_AssignToLocalVariable_01_Literal_01_ListOfInts()
+        {
+            CommandTest Test = new CommandTest();
+
+            // Arrange
+            string _methodSourceCode = "create list x of int {5, 2, 3};";
+
+            OALProgram programInstance = new OALProgram();
+            CDClass owningClass = programInstance.ExecutionSpace.SpawnClass("Class1");
+
+            CDMethod owningMethod = new CDMethod(owningClass, "Method1", "");
+            owningClass.AddMethod(owningMethod);
+
+            // Act
+            EXEScopeMethod methodScope = OALParserBridge.Parse(_methodSourceCode);
+            owningMethod.ExecutableCode = methodScope;
+            programInstance.SuperScope = methodScope;
+
+            EXEExecutionResult _executionResult = PerformExecution(programInstance);
+            EXEValueArray array = new EXEValueArray("integer[]");
+            array.InitializeEmptyArray();
+            array.AppendElement(new EXEValueInt(5), programInstance.ExecutionSpace);
+            array.AppendElement(new EXEValueInt(2), programInstance.ExecutionSpace);
+            array.AppendElement(new EXEValueInt(3), programInstance.ExecutionSpace);
+
+            // Assert
+            Test.Declare(methodScope, _executionResult);
+
+            Test.Variables
+                .ExpectVariable("x", array)
+                .ExpectVariable("self", new EXEValueReference(methodScope.OwningObject));
+
+            Test.PerformAssertion();
+        }
+
+        [Test]
+        public void HappyDay_01_AssignToLocalVariable_01_Literal_01_ListOfFloats()
+        {
+            CommandTest Test = new CommandTest();
+
+            // Arrange
+            string _methodSourceCode = "create list x of float {5.5, 2.2, 3.9};";
+
+            OALProgram programInstance = new OALProgram();
+            CDClass owningClass = programInstance.ExecutionSpace.SpawnClass("Class1");
+
+            CDMethod owningMethod = new CDMethod(owningClass, "Method1", "");
+            owningClass.AddMethod(owningMethod);
+
+            // Act
+            EXEScopeMethod methodScope = OALParserBridge.Parse(_methodSourceCode);
+            owningMethod.ExecutableCode = methodScope;
+            programInstance.SuperScope = methodScope;
+
+            EXEExecutionResult _executionResult = PerformExecution(programInstance);
+            EXEValueArray array = new EXEValueArray("real[]");
+            array.InitializeEmptyArray();
+            array.AppendElement(new EXEValueReal("5.5"), programInstance.ExecutionSpace);
+            array.AppendElement(new EXEValueReal("2.2"), programInstance.ExecutionSpace);
+            array.AppendElement(new EXEValueReal("3.9"), programInstance.ExecutionSpace);
+
+            // Assert
+            Test.Declare(methodScope, _executionResult);
+
+            Test.Variables
+                .ExpectVariable("x", array)
+                .ExpectVariable("self", new EXEValueReference(methodScope.OwningObject));
+
+            Test.PerformAssertion();
+        }
+
+        [Test]
+        public void HappyDay_01_AssignToLocalVariable_01_Literal_01_ListOfChars()
+        {
+            CommandTest Test = new CommandTest();
+
+            // Arrange
+            string _methodSourceCode = "create list x of char {\"ananas\", \"b\"};";
+
+            OALProgram programInstance = new OALProgram();
+            CDClass owningClass = programInstance.ExecutionSpace.SpawnClass("Class1");
+
+            CDMethod owningMethod = new CDMethod(owningClass, "Method1", "");
+            owningClass.AddMethod(owningMethod);
+
+            // Act
+            EXEScopeMethod methodScope = OALParserBridge.Parse(_methodSourceCode);
+            owningMethod.ExecutableCode = methodScope;
+            programInstance.SuperScope = methodScope;
+
+            EXEExecutionResult _executionResult = PerformExecution(programInstance);
+            EXEValueArray array = new EXEValueArray("string[]");
+            array.InitializeEmptyArray();
+            array.AppendElement(new EXEValueString("\"ananas\""), programInstance.ExecutionSpace);
+            array.AppendElement(new EXEValueString("\"b\""), programInstance.ExecutionSpace);
+
+            // Assert
+            Test.Declare(methodScope, _executionResult);
+
+            Test.Variables
+                .ExpectVariable("x", array)
+                .ExpectVariable("self", new EXEValueReference(methodScope.OwningObject));
+
+            Test.PerformAssertion();
+        }
+
+        [Test]
+        public void HappyDay_01_AssignToLocalVariable_01_Literal_01_ListOfBools()
+        {
+            CommandTest Test = new CommandTest();
+
+            // Arrange
+            string _methodSourceCode = "create list x of bool {FALSE, TRUE};";
+
+            OALProgram programInstance = new OALProgram();
+            CDClass owningClass = programInstance.ExecutionSpace.SpawnClass("Class1");
+
+            CDMethod owningMethod = new CDMethod(owningClass, "Method1", "");
+            owningClass.AddMethod(owningMethod);
+
+            // Act
+            EXEScopeMethod methodScope = OALParserBridge.Parse(_methodSourceCode);
+            owningMethod.ExecutableCode = methodScope;
+            programInstance.SuperScope = methodScope;
+
+            EXEExecutionResult _executionResult = PerformExecution(programInstance);
+            EXEValueArray array = new EXEValueArray("bool[]");
             array.InitializeEmptyArray();
             array.AppendElement(new EXEValueBool(false), programInstance.ExecutionSpace);
             array.AppendElement(new EXEValueBool(true), programInstance.ExecutionSpace);
