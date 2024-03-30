b691c36c465dd2f5d52f27e9b24a1daee9431786
Neltharion59 <lukawh5959@gmail.com>
2024-03-30 23:56:57 +0100

    Make write operations nonlocking towards console

--- Assets/Scripts/Visualization/Animation/ConsoleScheduler.cs
+++ Assets/Scripts/Visualization/Animation/ConsoleScheduler.cs
@@ -7,34 +7,47 @@ namespace Visualization.Animation
 {
     public class ConsoleScheduler
     {
-        private Queue<ConsoleRequest> RequestQueue;
+        private readonly Queue<ConsoleRequestRead> BlockingRequestQueue;
+        private readonly Queue<ConsoleRequestWrite> NonblockingRequestQueue;
         private bool Over;
 
         public ConsoleScheduler()
         {
-            this.RequestQueue = new Queue<ConsoleRequest>();
+            this.BlockingRequestQueue = new Queue<ConsoleRequestRead>();
+            this.NonblockingRequestQueue = new Queue<ConsoleRequestWrite>();
             this.Over = false;
         }
 
-        public IEnumerator Start()
+        public IEnumerator Start(MonoBehaviour animation)
         {
-            ConsoleRequest currentRequest;
+            animation.StartCoroutine(QueueLoop(BlockingRequestQueue));
+            animation.StartCoroutine(QueueLoop(NonblockingRequestQueue));
+            yield return new WaitForFixedUpdate();
+        }
+
+        private IEnumerator QueueLoop<T>(Queue<T> queue) where T : ConsoleRequest
+        {
+            T currentRequest;
             while (true)
             {
-                yield return new WaitUntil(() => RequestQueue.Any() || Over);
+                yield return new WaitUntil(() => queue.Any() || Over);
 
                 if (Over) { break; }
 
-                currentRequest = RequestQueue.Dequeue();
+                currentRequest = queue.Dequeue();
                 currentRequest.PerformRequest();
 
                 yield return new WaitUntil(() => currentRequest.Done);
             }
         }
 
-        public void Enqueue(ConsoleRequest request)
+        public void Enqueue(ConsoleRequestRead request)
         {
-            this.RequestQueue.Enqueue(request);
+            this.BlockingRequestQueue.Enqueue(request);
+        }
+        public void Enqueue(ConsoleRequestWrite request)
+        {
+            this.NonblockingRequestQueue.Enqueue(request);
         }
 
         public void Terminate()
